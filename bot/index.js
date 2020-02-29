6b3ac0a8bbbe7c933a0b02ade1c097e793735d0b
FMGordillo <facundomgordillo@gmail.com>
2020-02-29 19:36:35 -0300

    Logic!!

--- bot/index.js
+++ bot/index.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const assistantId = process.env.WATSON_ASSISTANT_ID;
 const watson = require("./watson");
 const shuffle = require("./random");
 const talkingToBot = require("./talking-to-bot");
 const netflixSuggestionsAPI = require("./netflix-suggestions");
 
 const prefix = "papurri";
+const assistantId = process.env.WATSON_ASSISTANT_ID;
 
 const ACTIONS = {
   // aiuda: help,
@@ -15,16 +15,17 @@ const ACTIONS = {
   random: shuffle
 };
 
-let sessionId = client.on("ready", () => {
+/**
+ * [
+ * { userId: {
+ * sessionId: String,
+ * lastMessage: Date
+ * }]
+ */
+let sessionHolder = [];
+
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  watson.createSession(
-    {
-      assistantId
-    },
-    (err, res) => {
-      sessionId = res.result.session_id;
-    }
-  );
 });
 
 client.on("message", async msg => {
@@ -42,26 +43,56 @@ client.on("message", async msg => {
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
   if (msg.content.startsWith(`${prefix} bot`)) {
+    console.log(sessionHolder);
     // Aca va nuestro Watson Assistant
-    watson
-      .message({
-        input: { text: msg.content.slice(prefix.length) },
-        assistantId,
-        sessionId
-      })
-      .then(res => {
-        console.log(res);
-        res.result.output.generic.forEach(response => {
-          msg.reply(response.text);
-        });
-      })
-      .catch(err => console.error("ERROR, ERROR beep", err));
 
-    // watson.message({
-    // input: { text: "What's the weather?" },
-    // assistantId: '<assistant id>',
-    // sessionId: '<session id>',
-    // })
+    /**
+     * 1) Chequeamos que el usuario que habló tenga un sessionid.
+     * if(!sessionId) 2.1 else 2.2
+     * 2.1) Creamos un sessionId
+     * 2.2) Chequeamos tiempo del mensaje actual con el último mensaje
+     * 2.2.1) if(tiempo_actual - tiempo_guardado > 1min) 2.1 else 3
+     * 3)
+     */
+
+    const author = msg.author.id;
+    let authorSessionId = sessionHolder.findIndex(val => val.id === author);
+    let authorSession =
+      authorSessionId !== -1 ? sessionHolder[authorSessionId] : {};
+
+    if (authorSession.sessionId) {
+      const now = Date.now();
+      // Seguimos con 2
+      console.log("AUTHOR", authorSession);
+      if (now - Date.parse(authorSession.lastMessage) > 60000) {
+        //TODO: Tomar la fecha de Discord
+        const newSession = await watson.createSession({ assistantId });
+        authorSession.sessionId = newSession.result.session_id;
+        authorSession.lastMessage = new Date();
+        sessionHolder[authorSessionId] = authorSession;
+        msg.reply("Te hacemos nueva sesion bb, arranca de cero");
+        return;
+      }
+      const removeThis = "papurri bot";
+      const text = msg.content.slice(removeThis.length);
+      await sendMessageToWatson(authorSession, text, msg);
+    } else {
+      const {
+        result: { session_id }
+      } = await watson.createSession({
+        assistantId
+      });
+      const sessionAuthor = {
+        id: author,
+        sessionId: session_id,
+        lastMessage: new Date() //TODO: Tomar la fecha de Discord
+      };
+      sessionHolder.push(sessionAuthor);
+      const removeThis = "papurri bot";
+      const text = msg.content.slice(removeThis.length);
+
+      await sendMessageToWatson(sessionAuthor, text, msg);
+    }
   }
 
   /**
@@ -105,6 +136,17 @@ client
   .catch(error => {
     console.error(`LOGIN() error`, error);
   });
+
+async function sendMessageToWatson(author, text, msg) {
+  const response = await watson.message({
+    input: { text },
+    sessionId: author.sessionId,
+    assistantId
+  });
+  response.result.output.generic.forEach(result => {
+    result.response_type === "text" && msg.reply(result.text);
+  });
+}
 
 exports.talkingToBot = talkingToBot;
 module.exports = client;
