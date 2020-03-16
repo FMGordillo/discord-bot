0cbc084f472f258e8a22f1ae50a5364da898c269
FMGordillo <facundomgordillo@gmail.com>
2020-03-16 17:35:17 -0300

    Testing push

--- bot/weather.js
+++ bot/weather.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const cityCode = "3433955"; //CABA
-const apiId = process.env.WEATHER_API_ID; //me da undefined ndea porque
+const apiId = process.env.WEATHER_API_ID;
 const unit = "metric";
 let url = `https://api.openweathermap.org/data/2.5/weather?id=${cityCode}&appid=${apiId}&units=${unit}`;
 
