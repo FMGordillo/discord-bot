5ea0e3ec39d6a7440f084f86b23683c8c3ad652c
FMGordillo <facundomgordillo@gmail.com>
2020-04-12 18:10:00 -0300

    :sparkles: :art: Weather API

--- bot/index.js
+++ bot/index.js
@@ -13,7 +13,7 @@ const assistantId = process.env.WATSON_ASSISTANT_ID;
 const ACTIONS = {
   // aiuda: help,
   nefli: netflixSuggestionsAPI,
-  random: shuffle
+  random: shuffle,
 };
 
 /**
@@ -29,32 +29,32 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", async msg => {
-  /**
-   * ESTO ES PARA LA JODA
-   */
+client.on("message", async (msg) => {
   // Exit!!
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
-  if(msg.content.startsWith("papurri clima")){
-    let weatherInfo= await weather.getTemperature();
-    msg.reply(`PARA QUE QUERES SABER ESO GORDITO SI ESTAS EN CUARENTENA DIOSS\n:
-              *Descripcion*: ${weatherInfo.description},
-              *Temperatura*: ${weatherInfo.temperature} C°,
-              *Térmica*: ${weatherInfo.feelsLike} C°,
-              *Humedad*: ${weatherInfo.humidity} %`);
+  if (msg.content.startsWith("papurri clima")) {
+    let weatherInfo = await weather.getCurrentWeather();
+    msg.reply(
+      `PARA QUE QUERES SABER ESO GORDITO SI ESTAS EN CUARENTENA DIOSS: ${weatherInfo}`,
+      {
+        embed: {
+          image: {
+            url: `http://openweathermap.org/img/wn/${weatherInfo.icon}@2x.png`,
+          },
+        },
+      }
+    );
     return;
   }
-  
 
   if (msg.content.startsWith(`${prefix} bot`)) {
-    
     const removeThis = "papurri bot";
     const text = msg.content.slice(removeThis.length);
 
-    const aut= await watson.updateSessionHolder(sessionHolder,msg)
-    if(aut){
-      await watson.sendMessageToWatson(aut,text,msg);
+    const aut = await watson.updateSessionHolder(sessionHolder, msg);
+    if (aut) {
+      await watson.sendMessageToWatson(aut, text, msg);
     }
     return;
   }
@@ -65,8 +65,8 @@ client.on("message", async msg => {
   const [_, action] = msg.content.split(" ");
   if (!action || action == "" || !Object.keys(ACTIONS).includes(action)) return;
   ACTIONS[action](msg)
-    .then(response => msg.reply(response))
-    .catch(error =>
+    .then((response) => msg.reply(response))
+    .catch((error) =>
       msg.reply(
         `Hubo un error con el comando **${action}**:
       \`${error.message}\``
@@ -94,11 +94,11 @@ client.on("message", async msg => {
 
 client
   .login(process.env.BOT_TOKEN)
-  .then(done => {
+  .then((done) => {
     const mainChannel = client.channels.get(process.env.MAIN_TEXT_CHANNEL);
     // console.log(mainChannel);
   })
-  .catch(error => {
+  .catch((error) => {
     console.error(`LOGIN() error`, error);
   });
 
